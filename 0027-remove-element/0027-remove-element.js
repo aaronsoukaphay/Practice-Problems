0575f4719ea45b7da9e6245d20cc25f802fce0be
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-10-26 14:21:58 -0700

    Time: 69 ms (5.6%), Space: 45.1 MB (5.2%) - LeetHub

---
+++ 0027-remove-element/0027-remove-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function(nums, val) {
+    nums.sort((a, b) => a-b)
+    console.log(nums)
+    let count = 0
+    nums.forEach((num, index) => {
+        if (val === num) {
+            count++
+        }
+    })
+    nums.forEach((num, index) => {
+        val === num && nums.splice(index, count)
+    })
+    console.log(count)
+};
