caef68d563a7639f885fbd8da0ec880648880dc6
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-10-30 22:05:11 -0700

    Time: 83 ms (13.07%), Space: 51.5 MB (58.68%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -6,15 +6,6 @@ var maxProfit = function(prices) {
     let lowest = prices[0]
     let lowestIndex = 0
     let profit = 0
-    // for (let i = 0; i < prices.length; i++) {
-    //     if (prices[i] < lowest){
-    //         lowest = prices[i]
-    //         lowestIndex = i
-    //         if (lowestIndex === prices.length - 1){
-    //             return 0
-    //         }
-    //     }
-    // }
     prices.forEach((price, i) => {
         if (price < lowest){
             lowest = price
@@ -24,8 +15,5 @@ var maxProfit = function(prices) {
             profit = price - lowest
         }
     })
-    console.log('lowest', lowest)
-    console.log('lowestIndex', lowestIndex)
-    console.log('profit', profit)
     return profit
 }
